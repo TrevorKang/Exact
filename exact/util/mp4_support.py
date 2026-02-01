de6cfe94d4980f125b1aa31405984df3e95e22e4
xingjian_kang <xingjian.kang@uni-wuerzburg.de>
2026-02-01 16:51:06 +0100

    scripts for MP4 support

---
+++ exact/util/mp4_support.py
@@ -0,0 +1,16 @@
+"""
+Scripts for MP4 files's support
+"""
+
+import numpy as np
+import cv2
+from PIL import Image
+
+from util.enums import FrameType
+
+class fileinfo:
+    pass
+
+class ReadMP4Dataset():
+    def __init__(self):
+        pass
